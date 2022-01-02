d94affa196ff9cd48497c76c2670553ed92f1217
Jason Liu <jliu2383@uwo.ca>
2022-01-02 18:44:37 -0500

    v3 started

---
+++ website-v3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BusinessCard from './BusinessCard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BusinessCard></BusinessCard>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ website-v3/src/BusinessCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './BusinessCard.css'
+
+class BusinessCard extends Component{
+    render(){
+        return(
+            <div className="b-card-wrap">
+                <div className="b-card">
+                    <h1>Hello I'm Jason</h1>
+                </div>  
+            </div>
+        )
+    }
+}
+
+export default BusinessCard;
